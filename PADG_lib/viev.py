5ba6ea28a6a2e2f98d19efe622ab39e3eda803d2
kubaz <kuba.zaluga@gmail.com>
2025-12-01 17:55:10 +0100

    feat: view dependency from user type

--- PADG_lib/viev.py
+++ PADG_lib/viev.py
@@ -1,10 +1,7 @@
 import tkinter
 from tkinter import ttk
 
-from win32cryptcon import szOID_ENCRYPTED_KEY_HASH
-
 from PADG_lib.controller import CemeteryFunctions
-from PADG_lib.model import cemetery_list
 
 class GUI(tkinter.Tk):
     def __init__(self):
@@ -17,51 +14,67 @@ class GUI(tkinter.Tk):
         self.cem_logic = CemeteryFunctions(self)
 
 
-        self.__create_map_viev()
+        self.__create_map_view()
         self.__create_widgets()
 
-    def __create_widgets(self):
-        self.frame_cem_list = tkinter.Frame(self)
-        self.frame_cem_list.grid(row=0, column=0, sticky="nw")
 
+    def __create_widgets(self):
+        self.frame_list = tkinter.Frame(self)
+        self.frame_list.grid(row=0, column=0, sticky="nw")
+
+        # Listbo
+        self.label_cem_list = tkinter.Label(self.frame_list, text="Lista obiektów")
+        self.label_cem_list.grid(row=0, column=0, columnspan=3)
+
+        self.label_choose_user = tkinter.Label(self.frame_list, text="Wybierz obiekt:")
+        self.label_choose_user.grid(row=1, column=0)
+        self.entry_choose_user = ttk.Combobox(self.frame_list,
+                                              values=["cmentarze"])
+
+        self.entry_choose_user.grid(row=1, column=1, columnspan=3,)
+
+        self.entry_choose_user.bind("<<ComboboxSelected>>", lambda event: self.__user_check())
+
+
+        self.listbox_list = tkinter.Listbox(self.frame_list)
+        self.listbox_list.grid(row=2, column=0, columnspan=3)
+        
+
+        self.button_remove = tkinter.Button(self.frame_list, text="Usuń cmentarz", command=self.cem_logic.remove_cemetery)
+        self.button_remove.grid(row=3, column=1)
+
+        self.button_edit = tkinter.Button(self.frame_list, text="Edytuj cmentarz", command=self.cem_logic.edit_cemetery)
+        self.button_edit.grid(row=3, column=2)
+        
+    def __create_cem_view(self):
         self.frame_cem_form = tkinter.Frame(self)
         self.frame_cem_form.grid(row=0, column=1, padx=10, sticky="w")
 
-        # Listbo
-        self.label_cem_list = tkinter.Label(self.frame_cem_list, text="Lista obiektów")
-        self.label_cem_list.grid(row=0, column=0, columnspan=3)
-        self.listbox_list = tkinter.Listbox(self.frame_cem_list)
-        self.listbox_list.grid(row=1, column=0, columnspan=3)
-
-        self.button_remove_cem = tkinter.Button(self.frame_cem_list, text="Usuń cmentarz", command=self.cem_logic.remove_cemetery)
-        self.button_remove_cem.grid(row=2, column=1)
-
-        self.button_insert_cem_info = tkinter.Button(self.frame_cem_list, text="Edytuj cmentarz", command=self.cem_logic.edit_cemetery)
-        self.button_insert_cem_info.grid(row=2, column=2)
-
-        # Form
         self.label_cem_form = tkinter.Label(self.frame_cem_form, text="Dodawanie cmentarza")
         self.label_cem_form.grid(row=0, column=0, columnspan=2)
 
         self.label_cem_name = tkinter.Label(self.frame_cem_form, text="Nazwa:")
         self.label_cem_name.grid(row=1, column=0)
         self.entry_cem_name = tkinter.Entry(self.frame_cem_form)
-        self.entry_cem_name.grid(row=1, column=1, pady = 2, sticky = "ew")
+        self.entry_cem_name.grid(row=1, column=1, pady=2, sticky="ew")
 
         self.label_cem_type = tkinter.Label(self.frame_cem_form, text="Rodzaj:")
         self.label_cem_type.grid(row=2, column=0)
-        self.entry_cem_type = ttk.Combobox(self.frame_cem_form, values=["komunalny","rzymskokatolicki", "ewangelicki", "żydowski", "prawosławny", "inny"])
-        self.entry_cem_type.grid(row=2, column=1, pady= 2, sticky="ew")
+        self.entry_cem_type = ttk.Combobox(self.frame_cem_form,
+                                           values=["komunalny", "rzymskokatolicki", "ewangelicki", "żydowski",
+                                                   "prawosławny", "inny"])
+        self.entry_cem_type.grid(row=2, column=1, pady=2, sticky="ew")
 
         self.label_cem_address = tkinter.Label(self.frame_cem_form, text="Adres:")
         self.label_cem_address.grid(row=3, column=0)
         self.entry_cem_address = tkinter.Entry(self.frame_cem_form)
-        self.entry_cem_address.grid(row=3, column=1, pady = 2, sticky= "ew")
+        self.entry_cem_address.grid(row=3, column=1, pady=2, sticky="ew")
 
-        self.button_cem_add = tkinter.Button(self.frame_cem_form, text="Dodaj cmentarz", command=self.cem_logic.add_cemetery)
+        self.button_cem_add = tkinter.Button(self.frame_cem_form, text="Dodaj cmentarz",
+                                             command=self.cem_logic.add_cemetery)
         self.button_cem_add.grid(row=4, column=0, columnspan=2)
-
-    def __create_map_viev(self):
+        
+    def __create_map_view(self):
         import tkintermapview
         self.frame_map = tkinter.Frame(self)
         self.frame_map.grid(row=2, column=0, pady=10, columnspan= 2)
@@ -70,6 +83,13 @@ class GUI(tkinter.Tk):
         self.map_widget.grid(row=1, column=0, sticky="sw")
         self.map_widget.set_position(52.0, 21.0)
         self.map_widget.set_zoom(6)
+
+    def __user_check(self):
+        if self.entry_choose_user.get() == "cmentarze":
+            self.user = "cemetery"
+            self.__create_cem_view()
+        else:
+            self.user = ""
 
     def get_entry(self) -> list:
         if self.user == "cemetery":
